e1b061c2ff865392c8babdc1a115f81ff406d959
DusekDan <dusekdan@gmail.com>
2025-07-10 23:24:47 +0100

    Add - Command usage helpers

--- HelperFunctions.lua
+++ HelperFunctions.lua
@@ -21,3 +21,31 @@ function HelperFunc.isInList(list, value)
 
     return false
 end
+
+function HelperFunc.NormalizeString(param)
+    if param == nil then
+        return ""
+    end
+
+    return string.lower(param)
+end
+
+function HelperFunc.ShowUsageForVerb(verb)
+    if verb == "mode" then
+        print("Usage /playtimer mode account OR /playtimer mode character")
+        return
+    end
+
+    print("Usage: /playtimer <time> (e.g., 10h30m10s, 10h, 30m)")
+    print("Usage: /playtimer add||reduce <time>")
+    print("Use /playtimer help to print a complete list of commands.")
+end
+
+function HelperFunc.ShowHelp()
+    print("Usage: /playtimer <time> (e.g., 10h30m10s, 10h, 30m)")
+    print(" /playtimer add <time> - add amount of <time> to your allowance")
+    print(" /playtimer reduce <time> - reduce the amount of <time> allowance")
+    print(" /playtimer pause - Pauses play timer")
+    print(" /playtimer mode account||character - switches between time allowance per account/character")
+
+end
